cba19c786dbc52cc6613b0149f4215e30160b95c
Scott Corgan <scottcorgan@gmail.com>
2016-10-27 10:22:54 -0400

    Char letter helpers

--- src/Keyboard/Combo.elm
+++ src/Keyboard/Combo.elm
@@ -2,14 +2,14 @@ module Keyboard.Combo
     exposing
         ( KeyCombo
         , Model
-        , Msg(..)
+        , Msg
         , update
         , init
         , subscriptions
-          --, fromCombo
         , combo1
         , combo2
         , combo3
+        , combo4
         , super
         , command
         , shift
@@ -17,7 +17,31 @@ module Keyboard.Combo
         , alt
         , option
         , a
+        , b
+        , c
+        , d
+        , e
+        , f
+        , g
+        , h
+        , i
+        , j
+        , k
+        , l
+        , m
+        , n
+        , o
+        , p
+        , q
+        , r
         , s
+        , t
+        , u
+        , v
+        , w
+        , x
+        , y
+        , z
         )
 
 {-| Provides helpers to call messages on the give keyboard combinations
@@ -26,7 +50,7 @@ module Keyboard.Combo
 
 ## Combo Helpers
 
-@docs combo1, combo2, combo3
+@docs combo1, combo2, combo3, combo4
 
 ## Modifiers
 
@@ -35,7 +59,7 @@ module Keyboard.Combo
 
 ## Letters
 
-@docs a, s
+@docs a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
 -}
 
 import Keyboard.Extra
@@ -57,6 +81,7 @@ type KeyCombo msg
     = KeyCombo Keyboard.Extra.Key msg
     | KeyCombo2 Keyboard.Extra.Key Keyboard.Extra.Key msg
     | KeyCombo3 Keyboard.Extra.Key Keyboard.Extra.Key Keyboard.Extra.Key msg
+    | KeyCombo4 Keyboard.Extra.Key Keyboard.Extra.Key Keyboard.Extra.Key Keyboard.Extra.Key msg
 
 
 
@@ -126,6 +151,12 @@ combo3 ( key1, key2, key3 ) msg =
     KeyCombo3 key1 key2 key3 msg
 
 
+{-| -}
+combo4 : ( Keyboard.Extra.Key, Keyboard.Extra.Key, Keyboard.Extra.Key, Keyboard.Extra.Key ) -> msg -> KeyCombo msg
+combo4 ( key1, key2, key3, key4 ) msg =
+    KeyCombo4 key1 key2 key3 key4 msg
+
+
 
 -- Modifier Keys
 
@@ -167,19 +198,189 @@ control =
 
 
 
--- English letters
+-- Letter helpers
 
 
-{-| -}
+{-| Letter helper for 'a'
+-}
 a : Keyboard.Extra.Key
 a =
     Keyboard.Extra.CharA
 
 
-{-| -}
+{-| Letter helper for 'b'
+-}
+b : Keyboard.Extra.Key
+b =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'c'
+-}
+c : Keyboard.Extra.Key
+c =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'd'
+-}
+d : Keyboard.Extra.Key
+d =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'e'
+-}
+e : Keyboard.Extra.Key
+e =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'f'
+-}
+f : Keyboard.Extra.Key
+f =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'g'
+-}
+g : Keyboard.Extra.Key
+g =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'h'
+-}
+h : Keyboard.Extra.Key
+h =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'i'
+-}
+i : Keyboard.Extra.Key
+i =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'j'
+-}
+j : Keyboard.Extra.Key
+j =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'k'
+-}
+k : Keyboard.Extra.Key
+k =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'l'
+-}
+l : Keyboard.Extra.Key
+l =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'm'
+-}
+m : Keyboard.Extra.Key
+m =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'n'
+-}
+n : Keyboard.Extra.Key
+n =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'o'
+-}
+o : Keyboard.Extra.Key
+o =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'p'
+-}
+p : Keyboard.Extra.Key
+p =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'q'
+-}
+q : Keyboard.Extra.Key
+q =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'r'
+-}
+r : Keyboard.Extra.Key
+r =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 's'
+-}
 s : Keyboard.Extra.Key
 s =
-    Keyboard.Extra.CharS
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 't'
+-}
+t : Keyboard.Extra.Key
+t =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'u'
+-}
+u : Keyboard.Extra.Key
+u =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'v'
+-}
+v : Keyboard.Extra.Key
+v =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'w'
+-}
+w : Keyboard.Extra.Key
+w =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'x'
+-}
+x : Keyboard.Extra.Key
+x =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'y'
+-}
+y : Keyboard.Extra.Key
+y =
+    Keyboard.Extra.CharA
+
+
+{-| Letter helper for 'z'
+-}
+z : Keyboard.Extra.Key
+z =
+    Keyboard.Extra.CharA
 
 
 
@@ -210,6 +411,9 @@ keyList combo =
         KeyCombo3 key1 key2 key3 msg ->
             [ key1, key2, key3 ]
 
+        KeyCombo4 key1 key2 key3 key4 msg ->
+            [ key1, key2, key3, key4 ]
+
 
 mapMsgFromCombos : Model msg -> Sub Msg -> Sub msg
 mapMsgFromCombos model sub =
@@ -217,13 +421,16 @@ mapMsgFromCombos model sub =
         |> Maybe.map
             (\combo ->
                 case combo of
-                    KeyCombo key msg ->
+                    KeyCombo _ msg ->
                         msg
 
-                    KeyCombo2 key1 key2 msg ->
+                    KeyCombo2 _ _ msg ->
                         msg
 
-                    KeyCombo3 key1 key2 key3 msg ->
+                    KeyCombo3 _ _ _ msg ->
+                        msg
+
+                    KeyCombo4 _ _ _ _ msg ->
                         msg
             )
         |> Maybe.map
